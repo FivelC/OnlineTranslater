a92f0f83cce6f9a7e83d4b0ecc93e44ddb4b852f
FivelC <elapin2006@gmail.com>
2026-07-24 16:04:30 +0300

    feat: insert into buffer and take clipboard

--- translator/src/main/java/Clipboard.java
+++ translator/src/main/java/Clipboard.java
@@ -1,7 +1,12 @@
 import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.desktop.SystemSleepEvent;
 import java.awt.event.KeyEvent;
+import java.awt.datatransfer.Clipboard;
+import java.io.IOException;
 
-class Clipboard{
+class TakeClipboard{
     private static void copy(){
         Robot robot = null;
         try {
@@ -16,8 +21,20 @@ class Clipboard{
     }
 
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, UnsupportedFlavorException, InterruptedException {
+        String new_res = null;
+        String old_res = null;
+        Clipboard old_clip = Toolkit.getDefaultToolkit().getSystemClipboard();
+        old_res = (String) old_clip.getData(DataFlavor.stringFlavor);
+        new_res = old_res;
         copy();
+        while(old_res.equals(new_res)) {
+            Thread.sleep(20);
+            Clipboard new_clip = Toolkit.getDefaultToolkit().getSystemClipboard();
+            new_res = (String) new_clip.getData(DataFlavor.stringFlavor);
+        }
+        System.out.println(new_res);
     }
 
 }
